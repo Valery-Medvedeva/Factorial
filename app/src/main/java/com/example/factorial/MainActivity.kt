931c231ea741cec6250fac33efbae49113b1ee94
Valery Medvedeva <smerdovava@gmail.com>
2024-03-15 10:21:17 +0400

    Initial commit

---
+++ app/src/main/java/com/example/factorial/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.factorial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.factorial.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
